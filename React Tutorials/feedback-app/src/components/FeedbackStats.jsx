d350aafdc7f6c363c77146853ef3bfe43bcf4966
vohn237 <83842875+vohn237@users.noreply.github.com>
2022-03-10 14:35:10 -0600

    removed gitignore files

--- React Tutorials/feedback-app/src/components/FeedbackStats.jsx
+++ React Tutorials/feedback-app/src/components/FeedbackStats.jsx
@@ -1,5 +1,16 @@
-function feedbackStats() {
-  return <div>Stats</div>;
+function feedbackStats({ feedback }) {
+  // Calculate rating average
+  let average =
+    feedback.reduce((acc, cur) => acc + cur.rating, 0) / feedback.length;
+
+  average = parseFloat(average.toFixed(1));
+
+  return (
+    <div className="feedback-stats">
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
+    </div>
+  );
 }
 
 export default feedbackStats;
