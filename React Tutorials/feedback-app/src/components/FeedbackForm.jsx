80a618ad014c155ea69603f798cce3b96f42166b
vohn237 <83842875+vohn237@users.noreply.github.com>
2022-03-11 20:45:14 -0600

    button features

--- React Tutorials/feedback-app/src/components/FeedbackForm.jsx
+++ React Tutorials/feedback-app/src/components/FeedbackForm.jsx
@@ -4,8 +4,20 @@ import Button from './shared/Button';
 
 function FeedbackForm() {
   const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
 
   const handleTextChange = (e) => {
+    if (text === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== '' && text.trim().length <= 10) {
+      setMessage('Text must be atleast 10 characters');
+    } else {
+      setBtnDisabled(false);
+      setMessage(null);
+    }
+
     setText(e.target.value);
   };
 
@@ -21,8 +33,11 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={text}
           />
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
